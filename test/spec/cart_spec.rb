5ed5e4c7fc9be306df9ed2f988d04d0170d088fb
Konrad Wright <KonradWright95@Gmail.com>
2016-12-22 13:41:24 -0800

    Added cart tests
    init context:
    has no items
    has a total of 0

--- test/spec/cart_spec.rb
+++ test/spec/cart_spec.rb
@@ -7,4 +7,10 @@ describe Cart do
   it 'has the name "John"' do
     expect(@cart.name).to eq "John"
   end
+  it 'has no items' do
+    expect(@cart.items.count).to eq 0
+  end
+  it 'has a total of 0' do
+    expect(@cart.total).to eq 0
+  end
 end
